d333b9f14038ca6f67618c446fb1ddde021cf77e
Loraklvn <68545884+Loraklvn@users.noreply.github.com>
2020-10-27 17:37:05 -0400

    idioma de antd cambiado a español (#109)

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import 'antd/dist/antd.css'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
+import { ConfigProvider } from 'antd';
 import store from './store'
 import Routes from './routes'
 import { defaultTheme } from './themes'
+import Spanish from 'antd/lib/locale/es_ES';
 
 function App(): React.ReactElement {
   return (
     <Provider store={store}>
       <ThemeProvider theme={defaultTheme}>
-        <Routes />
+        <ConfigProvider locale={Spanish}>
+          <Routes />
+        </ConfigProvider>
       </ThemeProvider>
     </Provider>
   )
